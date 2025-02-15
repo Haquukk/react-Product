8ed9c051ec83cafb3b86b7e3a96b7f86412dcd02
rizki alhaq <>
2025-02-15 18:02:38 +0700

    fix browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,28 @@
-import { HashRouter } from 'react-router-dom'
-import { About, Hero, LoadingScreen, Navbar, Works, Conclusion } from './Components'
-
+import { BrowserRouter } from "react-router-dom";
+import {
+    About,
+    Hero,
+    LoadingScreen,
+    Navbar,
+    Works,
+    Conclusion,
+} from "./Components";
 
 const App = () => {
+    return (
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+            <div className=" relative z-0 bg-gradient-to-b from-[#052222] to-black overflow-x-clip ">
+                <div className=" bg-gradient-to-b from-[#2c4c82]">
+                    <Navbar />
+                    <Hero />
+                </div>
+                <About />
+                <Works />
+                <Conclusion />
+            </div>
+            <LoadingScreen />
+        </BrowserRouter>
+    );
+};
 
-  return (
-    <HashRouter>
-      <div className=' relative z-0 bg-gradient-to-b from-[#052222] to-black overflow-x-clip '>
-
-        <div className=' bg-gradient-to-b from-[#2c4c82]'>
-          {/* progress 70% >>*/}
-          <Navbar />
-
-          {/* progress 100% >> */}
-          <Hero />
-
-        </div>
-        {/* progress 100% >> */}
-        <About />
-
-
-        {/* progress 85% >> */}
-        <Works />
-
-        {/* Progress 50% >> */}
-        <Conclusion/>
-      </div>
-
-      {/* progress 99% >> */}
-      <LoadingScreen /> 
-    </HashRouter>
-  )
-}
-
-export default App
+export default App;
