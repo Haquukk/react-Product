e3f724760ca44c1a191445d75d7e3430fa72b3ea
rizki alhaq <>
2025-02-15 16:45:49 +0700

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/React-Product-site',
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      minSize: 20000,
-      maxSize: 500000,
+    plugins: [react()],
+    base: "/React-Product-site",
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+            minSize: 20000,
+            maxSize: 500000,
+        },
     },
-  },
-  build: {
-    chunkSizeWarningLimit: 1000, // limit dalam kilobytes (KB)
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
-  },
-})
+    build: {
+        chunkSizeWarningLimit: 1000, // limit dalam kilobytes (KB)
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return id
+                            .toString()
+                            .split("node_modules/")[1]
+                            .split("/")[0]
+                            .toString();
+                    }
+                },
+            },
+        },
+    },
+});
